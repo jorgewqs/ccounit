e10b83becd7e325f03819bf6267b7f3e138d9430
Jorge Wendell <jorgewqs@gmail.com>
2017-10-24 21:08:43 -0300

    Add tipos de controle de acesso

--- cco/sca/models.py
+++ cco/sca/models.py
@@ -3,15 +3,20 @@ from django.forms import ModelForm
 
 
 class Setor(models.Model):
-    setor = models.CharField('Setor', max_length=10)
+    setor = models.CharField('Setor', max_length=100)
     descricao = models.TextField('Descrição', blank=True)
 
 SETOR_LISTA = [(s.setor, s.setor) for s in Setor.objects.all().order_by('setor')]
 
-class Sca(models.Model):
-    #setor = models.CharField('Setor', max_length=100)
-    setor = models.CharField('Setor', max_length=100, choices=SETOR_LISTA)
+class Tipo(models.Model):
     tipo = models.CharField('Tipo', max_length=100)
+    descricao = models.TextField('Descrição', blank=True)
+
+AUTO_LISTA = [(s.tipo, s.tipo) for s in Tipo.objects.all().order_by('tipo')]
+
+class Sca(models.Model):
+    setor = models.CharField('Setor', max_length=100, choices=SETOR_LISTA)
+    tipo = models.CharField('Tipo', max_length=100, choices=AUTO_LISTA)
     id_chamado = models.CharField('ID Chamado', max_length=12, null=True)
     nome = models.CharField('Nome', max_length=100)
     descricao = models.TextField('Descrição', blank=True)
